b8449aeaaafa3f1c7b3a083335703627d32fd720
Maria Filipe <8358160@formacao.iefp.pt>
2026-07-30 17:21:37 +0100

    Class cliente com destivar, ativar e mostrar

--- src/models/cliente.py
+++ src/models/cliente.py
@@ -1,15 +1,24 @@
 class Cliente:
 
-    def __init__(
-        self,
-        nome,
-        email,
-        telefone="",
-        id_cliente=None,
-        status=1
-    ):
-        self.id_cliente = id_cliente
+    def __init__(self, nome, telefone, email, nif, tipo_cliente="FISICA"):
+        self.id_cliente = None
         self.nome = nome
-        self.email = email
         self.telefone = telefone
-        self.status = status
+        self.email = email
+        self.nif = nif
+        self.tipo_cliente = tipo_cliente
+        self.ativo = True
+
+    def desativar(self):
+        self.ativo = False
+
+    def ativar(self):
+        self.ativo = True
+
+    def mostrar(self):
+        print("ID:", self.id_cliente)
+        print("Nome:", self.nome)
+        print("Telefone:", self.telefone)
+        print("Email:", self.email)
+        print("NIF:", self.nif)
+        print("Tipo:", self.tipo_cliente)
