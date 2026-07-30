cc99437b090a18ec2b9f628307c70454a4daf47d
Maria Filipe <8358160@formacao.iefp.pt>
2026-07-30 15:48:02 +0100

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.repositories.cliente_repository import listar
+
+
+def main():
+    print("=== TechService - Sistema de Gestão de Assistência Técnica ===")
+
+    print("\nClientes ativos:")
+
+    clientes = listar()
+
+    for item in clientes:
+        print(
+            item["id_cliente"],
+            item["nome"],
+            item["email"],
+            item["telefone"]
+        )
+
+
+if __name__ == "__main__":
+    main()
